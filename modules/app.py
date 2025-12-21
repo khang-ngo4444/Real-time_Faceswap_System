0deda46b64f214984f77351d9fd71816731b7698
tndot02 <nnguyen.tuan0205@gmail.com>
2025-12-21 21:55:11 +0700

    Adjust gitignore, add WIP mouth mask

--- modules/app.py
+++ modules/app.py
@@ -1,15 +1,28 @@
 import modules.capturer as capturer
 from modules.face_detector import FaceDetector
 from modules.face_swapper import FaceSwapper
-
 import cv2
+import numpy as np
+
+
+def mask_mouth(frame, face):
+    for i, (x, y) in enumerate(face.landmark_2d_106.astype(int)):
+        if 52 <= i <= 71:
+            # Mouth landmarks
+            cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
+        else:
+            # Other face landmarks
+            cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)        #
+
+    return frame
+
 
 def run():
     cap = capturer.Camera()
     face_detector = FaceDetector()
     face_swapper = FaceSwapper("models/inswapper_128.onnx")
 
-    source_img = cv2.imread("assets/23020016.jpg")
+    source_img = cv2.imread("assets/sample_5.jpg")
     source_face = face_detector.detect(img=source_img)[0]
 
     while True:
@@ -25,14 +38,10 @@ def run():
 
             # Face swap
             swapped_frame = face_swapper.swap(frame, target_face, source_face)
-            cv2.imshow("Swapped Face Raw Output", swapped_frame)
+            mouth = mask_mouth(frame, target_face)
 
-            # for x, y in target_face.landmark_2d_106.astype(int):
-            #     cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-            #
-            # # Bounding box
-            # x1, y1, x2, y2 = target_face.bbox.astype(int)
-            # cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            cv2.imshow("Mouth", mouth)
+            cv2.imshow("Swapped Face Raw Output", swapped_frame)
 
         if cv2.waitKey(1) == ord("q"):
             break
